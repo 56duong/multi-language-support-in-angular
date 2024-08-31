511f4a208db2a21cbbd0ffbcb786c02e051be74f
56duong <nguyenduong07122003@gmail.com>
2024-08-31 16:42:36 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'multi-language';
+  message: string = '';
+
+  constructor(
+    private translateService: TranslateService
+  ) {
+    translateService.setDefaultLang('en');
+    translateService.use(window.localStorage.getItem('lang') || 'en');
+  }
+
+
+  ngOnInit() {
+    this.translateService.onLangChange.subscribe(() => {
+      this.message = this.translateService.instant('message');
+    });
+  }
+
+
+  switchLang(lang: string) {
+    window.localStorage.setItem('lang', lang);
+    this.translateService.use(lang);
+  }
+
 }
